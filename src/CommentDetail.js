55406153a7bdedc655972c717540b6752ffb4240
cfue <curtis.ch.chat@gmail.com>
2020-02-09 19:51:47 -0700

    custom children

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,21 @@
 import React from "react";
-import faker from "faker";
+// import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = props => {
+  //   console.log(props)
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content"></div>
       <a href="/" className="author">
-        Sam
+        {props.author}
       </a>
       <div className="metadata">
-        <span className="date">Today at 6:44PM</span>
+        <span className="date">{props.timeAgo}</span>
       </div>
-      <div className="text">Nice blog post!</div>
+      <div className="text">{props.content}</div>
     </div>
   );
 };
